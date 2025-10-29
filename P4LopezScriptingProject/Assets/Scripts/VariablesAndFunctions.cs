5875eb0e3eecc9b5fbc1459a1f610cbe597cc46c
Core2QuadUser <719847@student.dusd.net>
2025-10-29 11:02:50 -0700

    Added Scene & Code

---
+++ P4LopezScriptingProject/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+
+    //create my integer variable
+    public int myInt = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run my variable tthru my function
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //create the multiply by two function
+    int MultiplyByTwo(int number)
+    {
+        //create a local variable
+        int result;
+        //multiplying num by 2
+        result = number * 2;
+        //return the value
+        return result;
+    }
+}
